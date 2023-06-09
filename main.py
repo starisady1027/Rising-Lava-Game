cdf5a8035dc107751e9fac25a34293a597bc6e90
Starisady1027 <curtischen0126@gmail.com>
2023-06-09 16:12:52 +0800

    6/9

--- main.py
+++ main.py
@@ -23,29 +23,51 @@ lava_image = pygame.image.load(os.path.join("lava.png"))
 lava = pygame.transform.scale(lava_image, (600,300))
 plr_position = (225,580)
 plr_image = pygame.image.load(os.path.join("player.png"))
-plr = pygame.transform.scale(plr_image, (40,80))
+plr_image_scaled = pygame.transform.scale(plr_image, (40,80))
 
-def basic_background(player):
+def basic_background():
     screen.fill(sky)
     screen.blit(cloud, (100,100))
     screen.blit(start_stone_platform, start_stone_platform_position)
     screen.blit(lava, lava_position)
-    screen.blit(plr, (player.x, player.y))
     pygame.display.update()
 
 class player(pygame.sprite.Sprite):
 
     def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.dir = "right"
+        self.x_vel = 0
+        self.y_vel = 0
+        self.animation_count = 0
 
-
-
-
-    def loop(keyPressed, player, velocity):
-        if keyPressed[pygame.K_a]:
-            player.x -= velocity
-        if keyPressed[pygame.K_d]:
-            player.x += velocity
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
     
+    def move_left(self, velocity):
+        self.x_vel = -velocity
+        if self.dir != "left":
+            self.dir = "left"
+            self.animation_count = 0
+    
+    def move_right(self, velocity):
+        self.x_vel = velocity
+        if self.dir != "right":
+            self.dir = "right"
+            self.animation_count = 0
+
+    def loop(self, fps, player, plr, screen):
+        self.move(self.x_vel, self.y_vel)
+        screen.blit(plr, (player.x, player.y))
+
+def moveHandler(player):
+    keys = pygame.key.getPressed()
+    player.x_vel = 0
+    if keys[pygame.K_A]:
+        player.move_left(velocity)
+    if keys[pygame.K_D]:
+        player.move_left(velocity)
 
 
 def main():
@@ -53,12 +75,16 @@ def main():
     clock = pygame.time.Clock()
     spacePressed = False
     global velocity
+    global plr_image_scaled
+    global screen
     while True:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        keyPressed = pygame.key.get_pressed()
-        basic_background(player)
+        player.loop(FPS, plr, plr_image_scaled, screen)
+        moveHandler(plr)
+        basic_background()
+
 if __name__ == "__main__":
     main()
