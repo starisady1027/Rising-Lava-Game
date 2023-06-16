590f1be60ee9aa5ef476118606fc7623efc59c2e
consleq <curtischen0126@gmail.com>
2023-06-16 22:56:16 +0800

    6/16 10pm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import random
+from threading import Thread
 
 pygame.init()
 
@@ -8,6 +9,8 @@ WIDTH, HEIGHT = (500, 900)
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Rising Lava")
 
+
+CLONELIST = []
 FPS = 60
 VELOCITY = 5
 GRAVITY = 1
@@ -25,7 +28,7 @@ lava_position = (-50,720)
 lava_image = pygame.image.load(os.path.join("lava.png"))
 lava = pygame.transform.scale(lava_image, (600,300))
 START_PLR_X = 225
-START_PLR_Y = 580
+START_PLR_Y = 600
 plr_image = pygame.image.load(os.path.join("player.png"))
 plr_image_scaled = pygame.transform.scale(plr_image, (40,80))
 
@@ -40,15 +43,15 @@ class stone():
     def __init__(self, x, y, width, height):
         super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
-        self.newRect = pygame.Rect(random.randint(100,240), random.randint(350,400), width, height)
+        self.newRect = pygame.Rect(random.randint(100,300), random.randint(360,400), width, height)
 
-    def draw(self, offset_y):
-        self.rect.y -= offset_y
-        pygame.draw.rect(SCREEN, GRAY, self.rect)
+    def draw(self):
+        CLONELIST.append(self.rect)
     
-    def drawNew(self, offset_y):
-        self.newRect.y -= offset_y
-        pygame.draw.rect(SCREEN, GRAY, self.newRect)
+    def drawNew(self):
+        CLONELIST.append(self.newRect)
+
+        
 
 
 
@@ -77,10 +80,10 @@ class player(pygame.sprite.Sprite):
     def move_right(self, velocity):
         self.x_vel = velocity
 
-    def loop(self, player, plr, offset_y):
+    def loop(self, player, plr,):
         self.y_vel += min(1, (self.fall_count / FPS) * GRAVITY)
         self.move(self.x_vel, self.y_vel)
-        SCREEN.blit(plr, (player.rect.x, player.rect.y + offset_y))
+        SCREEN.blit(plr, (player.rect.x, player.rect.y))
         self.fall_count += 1
 
     def landed(self):
@@ -92,9 +95,8 @@ class player(pygame.sprite.Sprite):
         self.fall_count = 0
         self.y_vel *= -1
 
-def collision(player, stone, dy):
-    stones = [stone.rect, stone.newRect]
-    for sto in stones:
+def vertical_collision(player, dy):
+    for sto in CLONELIST:
         if player.rect.colliderect(sto):
             if dy > 0:
                 player.rect.bottom = sto.top
@@ -103,24 +105,47 @@ def collision(player, stone, dy):
                 player.rect.top = sto.bottom
                 player.hit_head()
 
+def horizontal_collision(player, dx):
+    player.move(dx, 0)
+    player.update()
+    collided_object = []
+    for obj in CLONELIST:
+        if player.rect.colliderect(obj):
+            collided_object = obj
+            break
 
-def moveHandler(player, stone):
+    player.move(-dx, 0)
+    player.update()
+    return collided_object
+
+
+def moveHandler(player):
     keys = pygame.key.get_pressed()
     player.x_vel = 0
-    if keys[pygame.K_a] and player.rect.x > 0:
+    collide_left = horizontal_collision(player, -VELOCITY)
+    collide_right = horizontal_collision(player, VELOCITY)
+    if keys[pygame.K_a] and player.rect.x > 0 and not collide_left:
         player.move_left(VELOCITY)
-    if keys[pygame.K_d] and player.rect.x + player.rect.width < WIDTH:
+    if keys[pygame.K_d] and player.rect.x + player.rect.width < WIDTH and not collide_right:
         player.move_right(VELOCITY)
 
-    collision(player, stone, player.y_vel)
+    vertical_collision(player, player.y_vel)
 
-def Draw(player, stone, offset_y):
+def drawClone():
+    clone = pygame.Rect(random.randint(100,300), 150, 150, 30)
+    CLONELIST.append(clone)
+
+
+def Draw(player, stone):
     basic_background()
-    stone.draw(offset_y)
-    stone.drawNew(offset_y)
+    stone.draw()
+    stone.drawNew()
+    for sto in CLONELIST:
+        pygame.draw.rect(SCREEN, GRAY, sto)
     global plr_image_scaled
-    player.loop(player, plr_image_scaled, offset_y)
+    player.loop(player, plr_image_scaled)
     pygame.display.update()
+
 
 
 
@@ -128,9 +153,8 @@ def main():
     plr = player(START_PLR_X, START_PLR_Y, 40, 80)
     Stone = stone(START_STONE_X, START_STONE_Y, 150, 30)
     clock = pygame.time.Clock()
-    offset_y = 0
-    scroll_height = 200
-
+    counter = 0
+    run = False
     while True:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -139,11 +163,20 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and plr.jump_count < 1:
                     plr.jump()
-        Draw(plr, Stone, offset_y)
-        moveHandler(plr, Stone)
-        print(offset_y)
-        if ((plr.rect.top - offset_y <= scroll_height) and plr.y_vel < 0):
-            offset_y -= 0.1
+        moveHandler(plr)
+        Draw(plr, Stone)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_d]:
+            run = True
+        if run:
+            counter += 1
+            if counter > 100:
+                drawClone()
+                counter = 0
+            for sto in CLONELIST:
+                sto.y += 2
+
+
 
 if __name__ == "__main__":
     main()
